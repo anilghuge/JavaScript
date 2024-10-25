e8080bd880828ae9b33717fedb6a11009762f9fa
anilghuge <anilghuge780@gmail.com>
2024-10-25 22:25:03 +0530

    change image url

--- Session3/src/script/index.js
+++ Session3/src/script/index.js
@@ -1,6 +1,6 @@
 function bodyload(){
-   document.querySelector("img").src="public/images/DOM_hierarchy.png";
+   document.querySelector("img").src="public/images/DOM_Hierarchy.png";
    document.querySelector("#btnRegister").value="Regsiter";
    document.querySelector(".btn-login").value="Login";
-   document.querySelector("nav div img").src="public/images/DOM_hierarchy.png";
+   document.querySelector("nav div img").src="public/images/DOM_Hierarchy.png";
 }
